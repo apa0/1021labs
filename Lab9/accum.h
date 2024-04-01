3cc9edc7988cc0ceb02afaddbd8641aa13c1a73c
apa0 <adiazhe@clemson.edu>
2024-04-01 16:53:08 -0400

    added makefile/comments

--- Lab9/accum.h
+++ Lab9/accum.h
@@ -12,7 +12,9 @@
 //takes in integer vector
 template <typename T>
 T accum(std::vector<T> v) {
+    //using T() to intialize 
     T sum= T();
+    //adding all values
     for (const auto& x : v) {
         sum+=x;
     }
@@ -22,18 +24,16 @@ T accum(std::vector<T> v) {
 //take in string vector 
 template <typename T>
 T accum(std::vector<std::string> v) {
+    //using T() to initalize 
     T result=T();
     for (size_t i=0; i<v.size(); i++) {
         result+=v[i];
         //adds comma depending on if it is last element on list 
         if (i!=v.size()-1) {
                 result+=", ";
-        } else {
-            result+=" ";
         }
     }
     return result;
 
- 
 
 }
--- Lab9/main.cpp
+++ Lab9/main.cpp
@@ -10,27 +10,35 @@
 #include "accum.h"
 
 int main () {
+
+    //vector to hold integers
     std::vector<int>v;
+
+    //variables to store temp values
     int x;
     std::string temp;
+
+    //adding user input into vector 
     std::cout<<"Enter four numbers: ";
     for (int i=0; i<4; i++) {
         std::cin>>x;
         v.push_back(x);
     }
+    //calling accum function, passing vector v 
     std::cout<<"The sum of the numbers is "<<accum(v)<<std::endl;
     
-    v.clear();
-
+    //string vector 
     std::vector<std::string>v_string; 
     
-    std::cout<<"Enter four strings: \n";
+    //adding user input into vector 
+    std::cout<<"Enter four strings: ";
     for (int i=0; i<4; i++) {
         std::cin>>temp;
         v_string.push_back(temp);
     }
 
-    std::cout<<"The sum of the strings is: "<<accum<std::string>(v_string);
+    //calling accum function, passing v_string vector, specifying string return 
+    std::cout<<"The sum of the strings is "<<accum<std::string>(v_string);
 
 
     return 0;
