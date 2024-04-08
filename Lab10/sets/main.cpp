0652963ec22dc50c96a9823164479ea3470f3097
apa0 <adiazhe@clemson.edu>
2024-04-08 09:39:36 -0400

    added sets

---
+++ Lab10/sets/main.cpp
@@ -0,0 +1,37 @@
+//Angie Diaz CPSC 1021
+//Lab 10: Sets main.cpp
+//4/8/24
+//Desc: 
+
+
+/* In this program, the initial `std::vector` `stringData` contains strings, some
+of which are duplicates. These strings are then added to a `std::set` to remove
+duplicates. The unique elements are then transferred back to a new `std::vector`
+called `uniqueStrings`, which is then displayed.
+NOTE: you need to finish the code as specified in the comments
+NOTE: std::set was added to C++ in verion 17 so you may need to compile with -
+std=c++17 */
+
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    vector<string> stringData = {"apple", "banana", "apple", "orange", "banana",
+    "grape", "kiwi", "banana", "mango", "orange", "apple", "cherry", "mango", "kiwi",
+    "strawberry", "grape", "cherry", "apple", "orange", "banana"};
+    
+// Create a set to remove duplicates
+    set<string> stringSet(stringData.begin(), stringData.end());
+
+// Create a new vector with unique elements from the set
+    vector<string> uniqueStrings(stringSet.begin(), stringSet.end());
+// Display the unique elements in the new vector using a single space to separate each one. Use an endl at the end of the list.
+    for (auto element: uniqueStrings) {
+        cout<<element<<endl; 
+    }
+    return 0;
+}
