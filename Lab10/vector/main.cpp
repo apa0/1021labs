a47a2a830f54ec4190269250669630ab133ffed0
apa0 <adiazhe@clemson.edu>
2024-04-08 09:30:29 -0400

    added vector part

---
+++ Lab10/vector/main.cpp
@@ -0,0 +1,32 @@
+//Angie Diaz CPSC 1021
+//Lab 10: Vector main.cpp
+//4/8/24
+//Desc:Modified program to output each student's name and their AVERAGE score
+//rather than the individual scores
+
+#include <iostream>
+#include <vector>
+#include <utility>
+using namespace std; 
+
+int main() {
+    vector<pair<string, vector<int>>> studentData = {
+        {"Alice", {85, 90, 88, 92, 95}},
+        {"Bob", {78, 80, 85}},
+        {"Charlie", {92, 95, 90, 87, 76, 90}},
+        {"Diana", {80, 85}}
+    };
+    int sum=0;
+// Display the contents
+    for (const auto& student : studentData) {
+        cout << student.first << "'s average score: ";
+        for (int score : student.second) {
+            //cout << score << " ";
+            //adding each score
+            sum+=score; 
+        }
+        //getting average 
+        cout<<sum/student.second.size()<<endl;
+    }
+return 0;
+}
